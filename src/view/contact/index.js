5ebd8faf675bc0b49acecbdc5470fdc3253cf6a7
Laríssa Monteiro <55771616+Larissamonteiroo@users.noreply.github.com>
2020-03-04 23:16:26 -0300

    Atualização do Contato

--- src/view/contact/index.js
+++ src/view/contact/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Container, Input2, Body, Corpo, Form, Section, Textarea, Article } from './styles'
 
 import girl from './img/giphy.gif'
-import svg from './img/Svg.png'
-
 
 export default function Fale() {
 
@@ -11,9 +9,6 @@ export default function Fale() {
     <Corpo>
         <section>
           <h1>Contato</h1>
-          <svg viewBox="20 0 769 344" xmlns="http://www.w3.org/2000/svg">
-            <path d="M 272 0.0130308C 164.8 1.21303 46 85.1797 0 127.013L 0 342.013L 867 342.013L 867 6.51303C 779 0.013031 684.5 127.013 616.5 127.013C 548.5 127.013 406 -1.48697 272 0.0130308Z"/>
-          </svg>
         </section>
       <Form>
         <Container>
